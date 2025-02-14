e1bb9218c22b6bb1ac3a780fb344b08e9b280c58
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-02-14 03:12:57 -0800

    safe

--- tradeflexserver/routes/authRoute.js
+++ tradeflexserver/routes/authRoute.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const cors = require("cors");
 
 const corsOptions = {
-    origin: 'https://tradeflex.vercel.app', 
+    origin: 'https://tradevister.vercel.app', 
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   };
 
-  //https://tradevister.vercel.app
+  //http://localhost:5173
   
   router.use(cors(corsOptions));
   router.options('*', cors(corsOptions)); 
