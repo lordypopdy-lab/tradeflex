4a3e0552e3411352566faf069fe42a888a56fec4
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-02-15 06:52:20 -0800

    safe

--- tradeflexclient/src/pages/Deposite.jsx
+++ tradeflexclient/src/pages/Deposite.jsx
@@ -2,34 +2,30 @@ import React from 'react'
 import toast from 'react-hot-toast'
 import MainNavBar from '../components/MainNavBar'
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { convertNanotonsToTon } from "../utils/convertNanotonsToTon";
 import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
 
 
 const Deposite = () => {
     const [amount, setAmount] = useState('');
-    const navigate = useNavigate();
-    
     const [tonConnectUI] = useTonConnectUI();
 
     const handleDeposit = async () => {
-        if (amount > 0) {
-
+        const newAmount = convertNanotonsToTon(Number(amount));
+        if (amount >= 1) {
             await tonConnectUI.sendTransaction({
                 validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes
                 messages: [
                     {
-                        address: "0QD-SuoCHsCL2pIZfE8IAKsjc0aDpDUQAoo-ALHl2mje04A-",
-                        amount: '5000000',
+                        address: "UQDmVwpFwAQIFJO8T-KHWfZYqGYpmlzuhLM7iwV4jROQ1Ysf",
+                        amount: newAmount.toString(),
                     }
                 ]
             });
-            // console.log(`Depositing ${amount} TON...`);
-            // setTimeout(() => {
-            //     console.log("Success Deposit!");
-            //     navigate('#');
-            // }, 1000);
+        } else if (amount < 1) {
+            console.log(amount)
+            console.log(convertNanotonsToTon(Number(amount)))
+            toast.error("1 TON is the MIN deposite amount!");
         }
     };
 
@@ -43,7 +39,7 @@ const Deposite = () => {
     };
 
     const walletConnect = async () => {
-        toast.success("Wallet Connect Coming Soon!");
+        // toast.success("Wallet Connect Coming Soon!");
     }
 
     return (
@@ -54,30 +50,32 @@ const Deposite = () => {
                     <div className="main-panel m-0 w-100">
                         <div className="content-wrapper">
                             <div className="row">
+                            <div className="d-flex w-100 bg-secondary rounded justify-content-center align-items-center mb-3">
+                                        <div className="custom-ton-button p-2">
+                                            <TonConnectButton />
+                                        </div>
+                                    </div>
                                 <div className="p-4 max-w-md mx-auto text-center">
                                     <div>
                                         <h2 className="text-xl font-semibold">Enter Amount to Deposit</h2>
-                                        <input
-                                            type="number"
-                                            className="bg-transparent p-3 text-light border-0"
-                                            placeholder="Amount in TON"
-                                            value={amount}
-                                            onChange={(e) => setAmount(e.target.value)}
-                                        />
+                                        <p className="card-description"><span className="text-warning">Please make sure you connect your wallet before making deposite</span></p>
+                                        <div className="input-group mb-3">
+                                            <span className="input-group-text bg-secondary">$TON</span>
+                                            <input
+                                                type="number"
+                                                className="form-control bg-secondary text-light"
+                                                value={amount}
+                                                onChange={(e) => setAmount(e.target.value)}
+                                                aria-label="Amount (to the nearest dollar)" />
+                                            <span className="input-group-text bg-secondary">.00</span>
+                                        </div>
                                         <button
-                                            className="btn btn-primary m-2 p-3"
+                                            type="button"
                                             onClick={handleDeposit}
-                                        >
-                                            Deposit
-                                        </button>
+                                            className="btn btn-primary btn-lg">Send Transaction</button>
                                     </div>
                                 </div>
-                                <div style={{ borderRadius: "0px" }} className="col-xl-6 p-2 col-sm-6">
-                                    <button onClick={walletConnect} className='btn btn-secondary w-100'>
-                                        <div style={{ marginLeft: "20%" }} className="custom-ton-button">
-                                            <TonConnectButton />
-                                        </div>
-                                    </button>
+                                <div style={{ borderRadius: "0px" }} className="col-xl-6 col-sm-6">
                                     <div style={{ border: "none", borderRadius: "9px" }} className="card card-gradient">
                                         <div className="card-body card-gradient">
                                             <h5 onClick={() => handleCopy("bc1qd8x8mw8qvz5dy0gyknwsdptxpddc6dcf886ht7")}>bc1qd8x8mw8qvz5dy0gyknwsd...<i style={{ cursor: "pointer" }} className="fas fa-copy text-warning m-1"></i></h5>
--- tradeflexclient/src/utils/convertNanotonsToTon.jsx
+++ tradeflexclient/src/utils/convertNanotonsToTon.jsx
@@ -1,4 +1,4 @@
 export const convertNanotonsToTon = (nanotons) => {
-    return nanotons / 1e9;
+    return nanotons * 1e9;
   };
   
